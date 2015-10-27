0eef70dd7b16456978246b43488cafdd0fddd491
AndrewTelleria <atelleria08@gmail.com>
2015-10-27 15:25:13 -0700

    first commit

---
+++ javascript/game.js
@@ -0,0 +1,41 @@
+var num_correct = 0
+
+var guess = prompt("Is Barack Obama the 44th president of the U.S.? yes or no?");
+
+if (guess === "yes")
+{
+  alert("you are correct")
+  num_correct += 1
+}
+
+else {
+alert("you are wrong")
+}
+
+
+var guess = prompt("Am I from Salem originally? yes or no?");
+
+if (guess === "yes")
+{
+  alert("you are correct")
+  num_correct += 1
+}
+
+else {
+alert("you are wrong")
+}
+
+
+var guess = prompt("Is the Prime Minister of Australia Malcolm Turnbull? yes or no?");
+
+if (guess === "yes")
+{
+  alert("you are correct")
+  num_correct += 1
+}
+else {
+alert("you are wrong")
+}
+
+
+alert("Great job " + num_correct + "/3 is your final score")
